f7bb7b629ccf24887b328ae98bdae76eef8ca1ce
vrMars <vrMars@users.noreply.github.com>
2017-06-09 11:58:47 -0400

    Auth test v1

---
+++ app/src/main/java/com/neelakshbhatia/roome/BaseActivity.java
@@ -0,0 +1,34 @@
+package com.neelakshbhatia.roome;
+
+import android.app.ProgressDialog;
+import android.support.annotation.VisibleForTesting;
+import android.support.v7.app.AppCompatActivity;
+
+public class BaseActivity extends AppCompatActivity {
+
+    @VisibleForTesting
+    public ProgressDialog mProgressDialog;
+
+    public void showProgressDialog() {
+        if (mProgressDialog == null) {
+            mProgressDialog = new ProgressDialog(this);
+            mProgressDialog.setMessage("loading");
+            mProgressDialog.setIndeterminate(true);
+        }
+
+        mProgressDialog.show();
+    }
+
+    public void hideProgressDialog() {
+        if (mProgressDialog != null && mProgressDialog.isShowing()) {
+            mProgressDialog.dismiss();
+        }
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        hideProgressDialog();
+    }
+
+}
