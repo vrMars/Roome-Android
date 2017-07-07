2d675f8ee23d4f9718a90be751f630e9adc560d3
vrMars <vrMars@users.noreply.github.com>
2017-07-07 10:10:28 -0400

    removing google sign in for now as it was really buggy, may add it back in if need aries. for now, facebook is sufficient

--- app/src/main/java/com/neelakshbhatia/roome/Activity/LoginActivity.java
+++ app/src/main/java/com/neelakshbhatia/roome/Activity/LoginActivity.java
@@ -69,17 +69,15 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 public class LoginActivity extends BaseActivity implements
-        View.OnClickListener, View.OnTouchListener, GoogleApiClient.OnConnectionFailedListener {
+        View.OnClickListener, View.OnTouchListener {
 
     private static final String TAG = "EmailPassword";
 
     private TextView mStatusTextView;
-    private static final int RC_SIGN_IN = 9001;
     private TextView mDetailTextView;
     private EditText mEmailField;
     private CallbackManager callbackManager;
     private EditText mPasswordField;
-    private GoogleApiClient mGoogleApiClient;
     public static String account_email;
 
     // [START declare_auth]
@@ -101,7 +99,7 @@ public class LoginActivity extends BaseActivity implements
 
         callbackManager = CallbackManager.Factory.create();
         LoginButton loginButton = (LoginButton) findViewById(R.id.login_button);
-        loginButton.setReadPermissions("email","public_profile");
+        loginButton.setReadPermissions("email", "public_profile");
         loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
             @Override
             public void onSuccess(LoginResult loginResult) {
@@ -136,7 +134,8 @@ public class LoginActivity extends BaseActivity implements
             @Override
             public void onError(FacebookException error) {
 
-            } });
+            }
+        });
 
         View decorView = getWindow().getDecorView();
         // Hide the status bar.
@@ -148,18 +147,6 @@ public class LoginActivity extends BaseActivity implements
 
         Glide.with(this).load("http://www.designdpi.com/blog/images/blog/subtle-animated-gif-03.gif").into(imageView);
 
-        // Configure Google Sign In
-        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
-                .requestIdToken(getString(R.string.default_web_client_id))
-                .requestEmail()
-                .build();
-
-        // Build a GoogleApiClient with access to the Google Sign-In API and the
-        // options specified by gso.
-        mGoogleApiClient = new GoogleApiClient.Builder(this)
-                .enableAutoManage(this /* FragmentActivity */, this /* OnConnectionFailedListener */)
-                .addApi(Auth.GOOGLE_SIGN_IN_API, gso)
-                .build();
 
         // Views
         mStatusTextView = (TextView) findViewById(R.id.status);
@@ -200,7 +187,6 @@ public class LoginActivity extends BaseActivity implements
         });
 
         // Buttons
-        findViewById(R.id.sign_in_button).setOnClickListener(this);
         findViewById(R.id.email_sign_in_button).setOnClickListener(this);
         findViewById(R.id.email_create_account_button).setOnClickListener(this);
         findViewById(R.id.sign_out_button).setOnClickListener(this);
@@ -222,10 +208,10 @@ public class LoginActivity extends BaseActivity implements
         // Check if user is signed in (non-null) and update UI accordingly.
         FirebaseUser currentUser = mAuth.getCurrentUser();
         updateUI(currentUser);
-        if (currentUser!=null) {
+        if (currentUser != null) {
             account_email = currentUser.getEmail();
         }
-        startMain = new Intent (this, DailyNotificationsActivity.class);
+        startMain = new Intent(this, DailyNotificationsActivity.class);
     }
     // [END on_start_check_user]
 
@@ -284,9 +270,9 @@ public class LoginActivity extends BaseActivity implements
                             // Sign in success, update UI with the signed-in user's information
                             Log.d(TAG, "signInWithCredential:success");
                             FirebaseUser user = mAuth.getCurrentUser();
-                            Log.d("facebook",String.valueOf(user));
+                            Log.d("facebook", String.valueOf(user));
                             int uiOptions = View.SYSTEM_UI_FLAG_FULLSCREEN;
-                            startMain = new Intent (LoginActivity.this, DailyNotificationsActivity.class);
+                            startMain = new Intent(LoginActivity.this, DailyNotificationsActivity.class);
                             startActivity(startMain);
                         } else {
                             // If sign in fails, display a message to the user.
@@ -303,43 +289,6 @@ public class LoginActivity extends BaseActivity implements
                 });
     }
     // [END auth_with_facebook]
-
-
-    // [START auth_with_google]
-    private void firebaseAuthWithGoogle(GoogleSignInAccount acct) {
-        Log.d(TAG, "firebaseAuthWithGoogle:" + acct.getId());
-        // [START_EXCLUDE silent]
-        showProgressDialog();
-        // [END_EXCLUDE]
-
-        AuthCredential credential = GoogleAuthProvider.getCredential(acct.getIdToken(), null);
-        mAuth.signInWithCredential(credential)
-                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
-                    @Override
-                    public void onComplete(@NonNull Task<AuthResult> task) {
-                        if (task.isSuccessful()) {
-                            // Sign in success, update UI with the signed-in user's information
-                            Log.d(TAG, "signInWithCredential:success");
-                            FirebaseUser user = mAuth.getCurrentUser();
-                            account_email = user.getEmail();
-                            int uiOptions = View.SYSTEM_UI_FLAG_FULLSCREEN;
-                            startMain = new Intent (LoginActivity.this, DailyNotificationsActivity.class);
-                            startActivity(startMain);
-                        } else {
-                            // If sign in fails, display a message to the user.
-                            Log.w(TAG, "signInWithCredential:failure", task.getException());
-                            Toast.makeText(LoginActivity.this, "Authentication failed.",
-                                    Toast.LENGTH_SHORT).show();
-                            updateUI(null);
-                        }
-
-                        // [START_EXCLUDE]
-                        hideProgressDialog();
-                        // [END_EXCLUDE]
-                    }
-                });
-    }
-    // [END auth_with_google]
 
 
     private void signIn(final String email, String password) {
@@ -363,14 +312,14 @@ public class LoginActivity extends BaseActivity implements
                             // Sign in success, update UI with the signed-in user's information
                             Log.d(TAG, "signInWithEmail:success");
                             FirebaseUser user = mAuth.getCurrentUser();
-                            if (user!=null && user.isEmailVerified()==true) {
+                            if (user != null && user.isEmailVerified() == true) {
                                 account_email = user.getEmail();
-                                Log.d("haha","TRUE");
+                                Log.d("haha", "TRUE");
                                 View decorView = getWindow().getDecorView();
                                 // Hide the status bar.
                                 int uiOptions = View.SYSTEM_UI_FLAG_FULLSCREEN;
                                 decorView.setSystemUiVisibility(uiOptions);
-                                startMain = new Intent (LoginActivity.this, DailyNotificationsActivity.class);
+                                startMain = new Intent(LoginActivity.this, DailyNotificationsActivity.class);
                                 startActivity(startMain);
                             }
                         } else {
@@ -378,19 +327,19 @@ public class LoginActivity extends BaseActivity implements
                             Log.w(TAG, "signInWithEmail:failure", task.getException());
                             try {
                                 throw task.getException();
-                            } catch(FirebaseAuthWeakPasswordException e) {
+                            } catch (FirebaseAuthWeakPasswordException e) {
                                 Toast.makeText(LoginActivity.this,
                                         "Weak Password",
                                         Toast.LENGTH_SHORT).show();
-                            } catch(FirebaseAuthInvalidCredentialsException e) {
+                            } catch (FirebaseAuthInvalidCredentialsException e) {
                                 Toast.makeText(LoginActivity.this,
                                         "Invalid Email",
                                         Toast.LENGTH_SHORT).show();
-                            } catch(FirebaseAuthUserCollisionException e) {
+                            } catch (FirebaseAuthUserCollisionException e) {
                                 Toast.makeText(LoginActivity.this,
                                         "That User Already Exists",
                                         Toast.LENGTH_SHORT).show();
-                            } catch(Exception e) {
+                            } catch (Exception e) {
                                 Log.e(TAG, e.getMessage());
                             }
                             updateUI(null);
@@ -412,15 +361,6 @@ public class LoginActivity extends BaseActivity implements
         mAuth.signOut();
 
         LoginManager.getInstance().logOut();
-
-        // Google sign out
-        Auth.GoogleSignInApi.signOut(mGoogleApiClient).setResultCallback(
-                new ResultCallback<Status>() {
-                    @Override
-                    public void onResult(@NonNull Status status) {
-                        updateUI(null);
-                    }
-                });
     }
 
     private void sendEmailVerification() {
@@ -487,15 +427,14 @@ public class LoginActivity extends BaseActivity implements
             findViewById(R.id.email_password_buttons).setVisibility(View.GONE);
             findViewById(R.id.email_field).setVisibility(View.GONE);
             findViewById(R.id.password_field).setVisibility(View.GONE);
-            findViewById(R.id.sign_in_button).setVisibility(View.GONE);
+            findViewById(R.id.login_button).setVisibility(View.GONE);
             findViewById(R.id.signed_in_buttons).setVisibility(View.VISIBLE);
 
             if (user.isEmailVerified()) {
-                Log.d("Auth","true");
+                Log.d("Auth", "true");
                 findViewById(R.id.verify_email_button).setVisibility(View.GONE);
                 findViewById(R.id.proceed_to_list_button).setVisibility(View.VISIBLE);
-            }
-            else{
+            } else {
                 findViewById(R.id.verify_email_button).setVisibility(View.VISIBLE);
             }
 
@@ -506,7 +445,7 @@ public class LoginActivity extends BaseActivity implements
             findViewById(R.id.email_password_buttons).setVisibility(View.VISIBLE);
             findViewById(R.id.email_field).setVisibility(View.VISIBLE);
             findViewById(R.id.password_field).setVisibility(View.VISIBLE);
-            findViewById(R.id.sign_in_button).setVisibility(View.VISIBLE);
+            findViewById(R.id.login_button).setVisibility(View.VISIBLE);
             findViewById(R.id.signed_in_buttons).setVisibility(View.GONE);
         }
     }
@@ -520,17 +459,15 @@ public class LoginActivity extends BaseActivity implements
             signIn(mEmailField.getText().toString(), mPasswordField.getText().toString());
         } else if (i == R.id.sign_out_button) {
             signOut();
+            updateUI(null);
         } else if (i == R.id.verify_email_button) {
             sendEmailVerification();
-        }
-        else if (i == R.id.proceed_to_list_button){
-            startMain = new Intent (this, DailyNotificationsActivity.class);
+        } else if (i == R.id.proceed_to_list_button) {
+            startMain = new Intent(this, DailyNotificationsActivity.class);
             startActivity(startMain);
         }
-        else if(i == R.id.sign_in_button){
-            signInGoogle();
-        }
     }
+
     @Override
     public void startActivityForResult(Intent intent, int requestCode) {
         if (intent == null) {
@@ -543,43 +480,17 @@ public class LoginActivity extends BaseActivity implements
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
 
-        // Result returned from launching the Intent from GoogleSignInApi.getSignInIntent(...);
-        if (requestCode == RC_SIGN_IN) {
-            GoogleSignInResult result = Auth.GoogleSignInApi.getSignInResultFromIntent(data);
-            if (result.isSuccess()) {
-                // Google Sign In was successful, authenticate with Firebase
-                GoogleSignInAccount account = result.getSignInAccount();
-                firebaseAuthWithGoogle(account);
-            } else {
-                // Google Sign In failed, update UI appropriately
-                // [START_EXCLUDE]
-                updateUI(null);
-                // [END_EXCLUDE]
-            }
-        }
-
         //Return result to Facebook SDK
         callbackManager.onActivityResult(requestCode, resultCode, data);
 
     }
 
-    private void signInGoogle() {
-        Intent signInIntent = Auth.GoogleSignInApi.getSignInIntent(mGoogleApiClient);
-        startActivityForResult(signInIntent, RC_SIGN_IN);
-    }
-
     @Override
     public boolean onTouch(View v, MotionEvent event) {
-        if (mEmailField.getVisibility()==View.GONE) {
+        if (mEmailField.getVisibility() == View.GONE) {
             InputMethodManager imm = (InputMethodManager) getSystemService(INPUT_METHOD_SERVICE);
             imm.hideSoftInputFromWindow(getCurrentFocus().getWindowToken(), 0);
             return true;
-        }
-        else return false;
-    }
-
-    @Override
-    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
-
+        } else return false;
     }
 }
